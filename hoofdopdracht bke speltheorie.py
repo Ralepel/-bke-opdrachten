ee86c99184b13b18f5b7cd079769be9f45cbc7f1
Ralepel <45490824+Ralepel@users.noreply.github.com>
2021-01-05 23:29:52 +0100

    mySmarterAgent geprogrammeerd
    Zo gemaakt dat hij bijna elk potje wint

--- hoofdopdracht bke speltheorie.py
+++ hoofdopdracht bke speltheorie.py
@@ -4,3 +4,16 @@ from bke import *
 class MyRandomAgent(EvaluationAgent):
     def evaluate(self, board, my_symbol, opponent_symbol):
         return random.randint(1,500)
+    
+class MySmarterAgent(EvaluationAgent):
+    def evaluate(self, board, my_symbol, opponent_symbol):
+        getal = 1
+        if board[4] == my_symbol:
+            getal +=  2
+        if can_win(board, my_symbol):
+            getal += 3
+        if is_winner(board, my_symbol):
+            getal = 10
+        elif can_win(board, opponent_symbol):
+            getal = getal - 10
+        return getal
